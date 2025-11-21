a03907f7b26f3ecf68d51a70c60032165853e573
Ahmed-dev <ahmedsharif90@icloud.com>
2025-11-21 18:43:57 -0500

    Added KPI cards to Dashboard

---
+++ client/Mobility-Weather-Dashboard/src/components/KPICard.jsx
@@ -0,0 +1,13 @@
+
+
+function KPICard({title, value}){
+
+    return (
+        <>
+            <h3>{title}</h3>
+            <p>{value}</p>
+        </>
+    )
+}
+
+export default KPICard                                                                                                                                                                       
--- client/Mobility-Weather-Dashboard/src/pages/Dashboard.jsx
+++ client/Mobility-Weather-Dashboard/src/pages/Dashboard.jsx
@@ -2,8 +2,12 @@ import { useEffect } from "react"
 import axios from "axios"
 import { useDispatch, useSelector } from "react-redux"
 import { setWeather, setTraffic, setRefresh } from "../redux/slices/dataSlice"
+import KPICard from "../components/KPICard.jsx"
 
 function Dashboard() {
+  const weatherRes = {}
+  const trafficRes = {}
+  const refreshRes = {}
   const dispatch = useDispatch()
   const weather = useSelector((state) => state.data.weather)
   const traffic = useSelector((state) => state.data.traffic)
@@ -11,7 +15,7 @@ function Dashboard() {
 
   useEffect(async () => {
     try{
-      const weatherRes = await axios.get("http://localhost:5000/api/weather/11230")
+      weatherRes = await axios.get("http://localhost:5000/api/weather/11230")
       dispatch(setWeather(weatherRes.data))
     }
     catch(err){ 
@@ -19,7 +23,7 @@ function Dashboard() {
     }
 
     try{
-      const trafficRes = await axios.get("http://localhost:5000/api/traffic/brooklyn")
+      trafficRes = await axios.get("http://localhost:5000/api/traffic/brooklyn")
       dispatch(setTraffic(trafficRes.data))
     }
     catch(err){
@@ -29,13 +33,13 @@ function Dashboard() {
 
   async function handleRefresh(){
     try{
-      const weatherRes = await axios.get("http://localhost:5000/api/weather/11230")
+      weatherRes = await axios.get("http://localhost:5000/api/weather/11230")
       dispatch(setWeather(weatherRes.data))
 
-      const trafficRes = await axios.get("http://localhost:5000/api/traffic/brooklyn")
+      trafficRes = await axios.get("http://localhost:5000/api/traffic/brooklyn")
       dispatch(setTraffic(trafficRes.data))
 
-      const refreshRes = await axios.post("http://localhost:5000/api/refresh-data")
+      refreshRes = await axios.post("http://localhost:5000/api/refresh-data")
       dispatch(setRefresh(refreshRes.data)) 
 
       if(refreshRes.data.success){
@@ -52,18 +56,29 @@ function Dashboard() {
   return (
     <>
       <h1>DASHBOARD</h1>
+
       {refresh?.success && (
         <p style={{color:"green", fontWeight:"bold"}}>
           {refresh.success}
         </p>
       )}
+
+      <KPICard title="Temperature (F)" value={weatherRes.temp}/>
+      <KPICard title="Condition" value={weatherRes.main}/>
+      <KPICard title="Humidity (%)" value={weatherRes.humidity}/>
+      
+      <KPICard title="Avg Traffic Speed" value={trafficRes.avg_speed}/>
+      <KPICard title="Congestion Level" value={trafficRes.congestion_level}/>
+
+      <KPICard title="Last Updated" value={refreshRes.updatedAt}/>
+
       <h2>Weather</h2>
       <pre>{JSON.stringify(weather, null, 2)}</pre>
 
       <h2>Traffic</h2>
       <pre>{JSON.stringify(traffic, null, 2)}</pre>
 
-      <button onCLick = {handleRefresh}>Refresh</button>
+      <button onCLick = {handleRefresh}>Refresh</button>    
     </>
   );
 }
